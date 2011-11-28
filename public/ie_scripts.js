3900659e04e8c66966d7580d6c07999b9630ad7f
Erik Flood <ferik@student.chalmers.se>
2011-11-28 14:43:08 +0100

    IE support

--- public/ie_scripts.js
+++ public/ie_scripts.js
@@ -22,16 +22,21 @@ var fingerprint =
 
 function addIEPlugin(plugObj) {
     if (window.ActiveXObject) {
-	var control = null;
-	try {
-	    control = new ActiveXObject(plugObj.id);
-	} catch (e) {
+	var i = 0;
+	control = null;
+	while (control == null) {
+	    try {
+		control = new ActiveXObject(plugObj.ids[i]);
+	    } catch (e) {
+	    }
+	    i++;
 	}
 	if (control) {
+	    break;
 	    isInstalled = true;
 	    version = control.versionInfo;
 	    fingerprint.plugins.push({'name' : plugObj.name,
-				     'version' : version});
+				      'version' : plugObj.getVersion(control,plugObj.ids[i])});
 	}
     } 
 }
@@ -48,9 +53,44 @@ $(function()
 	color_depth :  screen.colorDepth 
     };
 
-    var iePlugins = [{'name' : 'Windows Media Player', 'id' : 'WMPlayer.OCX'},
-		     {'name' : 'Adobe Reader', 'id' : 'PDF.PdfCtrl'}
-		    ];
+    var ieplugins = [
+	{name: 'Quicktime',
+	 ids: ["QuickTimeCheckObject.QuickTimeCheck.1", "QuickTime.QuickTime"],
+	 getVersion: function(obj, progid) {return obj.QuickTimeVersion.toString(16).replace(/^(.)(.)(.).*/, "$1.$2.$3")}
+	},
+	{name: 'Acrobat',
+	 ids: ["PDF.PdfCtrl.7", "PDF.PdfCtrl.6", "PDF.PdfCtrl.5", "PDF.PdfCtrl.4", "PDF.PdfCtrl.3", "AcroPDF.PDF.1"],
+	 getVersion: function(obj, progid) {return progid.replace(/^[a-zA-Z.]+\.([0-9][0-9.]*)/, "$1");}
+	},
+	{name: 'RealPlayer',
+	 ids: ["RealPlayer.RealPlayer(tm) ActiveX Control (32-bit)", "RealVideo.RealVideo(tm) ActiveX Control (32-bit)", "rmocx.RealPlayer G2 Control"],
+	 getVersion: function(obj, progid) {return obj.GetVersionInfo();}
+	},
+	{name: 'Flash',
+	 ids: ["ShockwaveFlash.ShockwaveFlash.9", "ShockwaveFlash.ShockwaveFlash.8.5", "ShockwaveFlash.ShockwaveFlash.8", "ShockwaveFlash.ShockwaveFlash.7", "ShockwaveFlash.ShockwaveFlash.6", "ShockwaveFlash.ShockwaveFlash.5", "ShockwaveFlash.ShockwaveFlash.4"],
+	 getVersion: function(obj, progid) {return obj.GetVariable("$version").replace(/[a-zA-Z ]*([0-9,]+)/, "$1").replace(/,/g, '.');}
+	},
+	{name: 'Adobe SVG',
+	 ids: ["Adobe.SVGCtl"],
+	 getVersion: function(obj, progid) {return obj.getSVGViewerVersion().replace(/[a-zA-Z; ]*([0-9.]+)/, "$1")};
+	},
+	{name: 'Windows Media Player',
+	 ids: ["WMPlayer.OCX", "MediaPlayer.MediaPlayer.1"],
+	 getVersion: function(obj, progid) {return obj.versionInfo;}
+	},
+	{name: 'DivX',
+	 ids: ["npdivx.DivXBrowserPlugin.1", "npdivx.DivXBrowserPlugin"],
+	 getVersion: function(obj, progid) {return obj.GetVersion()}
+	},
+	{name: 'WPFe (Silverlight)',
+	 ids: ["AgControl.AgControl"],
+	 getVersion: function(obj, progid) {return obj.settings.version}
+	},
+	{name: 'MSXML',
+	 ids: ["MSXML2.DOMDocument.6.0", "MSXML2.DOMDocument.5.0", "MSXML2.DOMDocument.4.0", "MSXML2.DOMDocument.3.0"],
+	 getVersion: function(obj, progid) {return progid.replace(/^[a-zA-Z.2]+\.([0-9]+\.[0-9.]+)/, "$1");}
+	}
+    ];
     
     for (var i = 0; i < iePlugins.length; i++) {
 	addIEPlugin(iePlugins[i]);
