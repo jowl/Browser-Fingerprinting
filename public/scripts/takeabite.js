f174ea46fc76d93e255de55c82170b76eb05659f
Joel Karlsson <joel@kogito.se>
2012-01-11 10:25:44 +0100

    optimization

--- public/scripts/takeabite.js
+++ public/scripts/takeabite.js
@@ -9,9 +9,8 @@ function init(){
     Fingerprint.events.add(Fingerprint.initFlash,['/scripts/Fonts.swf'])
                       .add(Fingerprint.updatePlugins,[])
                       .add(Fingerprint.updateCSSFonts,[cssFontList])
-	              .add(Fingerprint.updateRTT,[10,'/time']);
-
-    $('#cookie').load(function(){Fingerprint.events.run();});
+	              .add(Fingerprint.updateRTT,[10,'/time'])
+	              .run();
 
 }
 
